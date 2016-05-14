0f6518975ce64632352cc843464aa788f7ee027b
qq519043202 <519043202@qq.com>
2016-05-14 14:51:22 +0800

    sy1

--- sy1/sy1-2.R
+++ sy1/sy1-2.R
@@ -1,6 +1,10 @@
-#
-x<-runif(10000,0,100)
+# 随机生成0-100的10000个数字，并输出（以print 一个list是实现）
+# 两份实现方法，一份大部分用系统自带的函数
+# 另一份大多是自己写的函数，可能有部分不准
 
+x<-runif(10000,0,100) # 生成10000个0-100的随机数
+
+# 计算平均数
 m1 <- function(x){
   sum = 0
   for(n in x){
@@ -8,6 +12,8 @@ m1 <- function(x){
   }
   return (sum/length(x))
 }
+
+# 计算方差
 var1 <- function(x){
   mean = m1(x)
   sum = 0
@@ -16,14 +22,18 @@ var1 <- function(x){
   }
   return(sum/length(x))
 }
+
+# 计算标准差
 sd1<- function(x)
 {
   return(var1(x)**0.5)
 }
+# 计算变异系数
 cv1 <- function(x)
 {
   return(sd1(x)/m1(x))
 }
+# 计算中位数
 m2 <- function(x)
 {
   x = sort(x)
@@ -34,10 +44,13 @@ m2 <- function(x)
     return(x[ceiling(length(x)/2)])
   }
 }
+# 计算四分位数
 q4 <-function(x){
   x = sort(x)
   return(list(x[length(x)/4],x[length(x)/4*3]))
 }
+
+# 自己的计算统计量函数
 analysis_me <- function(x)
 {
   mea_num = m1(x)
@@ -54,6 +67,7 @@ analysis_me <- function(x)
   return(data)
 }
 
+# 集成R函数的计算统计量函数
 analysis <- function(x){
   
   mea_num = mean(x)
@@ -72,6 +86,9 @@ analysis <- function(x){
   return(data)
 }
 
+# 比较花费时间，结果在报告中
+# print(analysis(x))
 system.time(print(analysis(x)))
+# print(analysis_me(x))
 system.time(print(analysis_me(x)))
 
--- sy1/sy1-3.R
+++ sy1/sy1-3.R
@@ -1,14 +1,17 @@
-#attach(mtcars)
-#layout(matrix(c(1,2,3,4,5,6), 2, 3, byrow = TRUE))
+# 偷懒不写出各种分布了。
+# 控制布局函数
+# attach(mtcars)
+# layout(matrix(c(1,2,3,4,5,6), 2, 3, byrow = TRUE))
+
 f1 <- function(n){
   a <- c()
   for(i in 1:10000){
-    x1<-rbinom(n,1,0.5)
+    x1<-rbinom(n,1,0.5) # 更改下这个函数生成不同的分布的随机数
     a <-c(a,mean(x1))
   }
   return(a)
 }
-
+# 画出不同样本容量的直方图
 draw_hist <- function(){
    hist(f1(3))
    hist(f1(5))
