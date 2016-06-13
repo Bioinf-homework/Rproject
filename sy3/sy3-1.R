ec277054f228d564e28084873cf677655ad79837
tmn07 <519043202@qq.com>
2016-06-13 20:17:08 +0800

    3.1-finish

--- sy3/sy3-1.R
+++ sy3/sy3-1.R
@@ -1,4 +1,5 @@
-setwd("D:/Programs/Rproject/sy3")
+# setwd("D:/Programs/Rproject/sy3")
+setwd("/media/tmn07/3AD6217DD6213A91/Programs/Rproject/sy3")
 
 # x1 <- rnorm(10,10,1)
 # x2 <- rnorm(10,10.2,1)
@@ -16,9 +17,9 @@ data = data.frame(X,A)
 # 方差检验
 bartlett.test(data$X, data$A)
 
-
 # 单因素分析
 data.aov = aov(X~A,data)
+
 # 输出方差分析表
 summary(data.aov)
 
@@ -33,3 +34,32 @@ x1 <- rnorm(300,10,1)
 x2 <- rnorm(300,10.2,1)
 
 t.test(x1,x2)
+
+# 
+myaov <- function(X,A){
+  m <- mean(X)
+  a1 <- X[1:10]
+  a2 <- X[11:20]
+  a3 <- X[21:30]
+  a4 <- X[31:40]
+  SSt <- sum((X-m)**2)
+  vt <- length(X)-1
+  a1m = mean(a1)
+  a2m = mean(a2)
+  a3m = mean(a3)
+  a4m = mean(a4)
+  am <- c(a1m,a2m,a3m,a4m)
+  SSj <- 10*sum((am - mean(am))**2)
+  vj <- length(levels(A))-1
+  Mj <- SSj/vj
+  SSn <- SSt - SSj
+  vn <- vt-vj
+  Mn <- SSn/vn
+  F <- Mj/Mn
+  
+  p = pf(F,vj,vn)
+  cat("F=")
+  cat(F)
+  cat("\nP=")
+  cat(p)
+}
--- sy3/sy3-2.R
+++ sy3/sy3-2.R
@@ -20,26 +20,54 @@ x42 <- rnorm(10,13.2,1.5)
 x43 <- rnorm(10,13.4,1.5)
 x44 <- rnorm(10,13.6,1.5)
 
-
 X = c(x11,x12,x13,x14,x21,x22,x23,x24,x31,x32,x33,x34,x41,x42,x43,x44)
 
 A = gl(4,40)
 B = gl(4,10,160)
 
+y11 <- rnorm(1,10,1)
+y12 <- rnorm(1,10.2,1)
+y13 <- rnorm(1,10.4,1)
+y14 <- rnorm(1,10.6,1)
+y21 <- rnorm(1,8,1)
+y22 <- rnorm(1,8.2,1)
+y23 <- rnorm(1,8.4,1)
+y24 <- rnorm(1,8.6,1)
+y31 <- rnorm(1,9,2)
+y32 <- rnorm(1,9.2,2)
+y33 <- rnorm(1,9.4,2)
+y34 <- rnorm(1,9.6,2)
+y41 <- rnorm(1,13,1.5)
+y42 <- rnorm(1,13.2,1.5)
+y43 <- rnorm(1,13.4,1.5)
+y44 <- rnorm(1,13.6,1.5)
+# no repeat
+Y = c(y11,y12,y13,y14,y21,y22,y23,y24,y31,y32,y33,y34,y41,y42,y43,y44)
+A2 = gl(4,4)
+B2 = gl(4,1,16)
+
+
+
 data = data.frame(X,A,B)
+data2 = data.frame(Y,A2,B2)
+
+# no interaction
+# data.aov <- aov(X~A+B,data) 
+# summary(data.aov)
+
+# draw..
+# op <- par(mfrow = c(1,2))
+# plot(X~A+B,data)
+# op <- par(mfrow = c(1,2))
+# plot(Y~A2+B2,data)
 
 
-data.aov <- aov(X~A+B,data) 
+# data.aov2 <- aov(X~A+B+A:B,data)
+data.aov <- aov(X~A*B,data)
 summary(data.aov)
 
-tukey.add.test(X,A,B)
 
-
-op <- par(mfrow = c(1,2))
-plot(X~A+B,data)
-
-data.aov2 <- aov(X~A+B+A:B,data)
-# data.aov2 <- aov(X~A*B,data)
-summary(data.aov2)
-
-# 交互检验？？
+data2.aov <- aov(Y~A2*B2,data2)
+summary(data2.aov)
+# 交互检验
+tukey.add.test(Y,A2,B2)
